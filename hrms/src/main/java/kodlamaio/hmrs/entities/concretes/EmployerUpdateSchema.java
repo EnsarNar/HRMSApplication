6c593075d0e92352a9e569cfe6b55148fe129f34
EnsarNar <ensarnar1@gmail.com>
2021-08-27 22:22:52 +0300

    27.08.2021 entity of EmployerUpdateSchema added

---
+++ hrms/src/main/java/kodlamaio/hmrs/entities/concretes/EmployerUpdateSchema.java
@@ -0,0 +1,45 @@
+package kodlamaio.hmrs.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="employer_update_schemas")
+public class EmployerUpdateSchema {
+
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="company_name")
+	private String companyName;
+	
+	@Column(name="phone_number")
+	private String phoneNumber;
+	
+	@Column(name="web_adress")
+	private String webAdress;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="password_repeat")
+	private String passwordRepeat;
+	
+	@Column(name="email")
+	private String email;
+	
+}
